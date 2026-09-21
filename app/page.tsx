cede7b9def717cb7463ad537eb09e8fd5ec74e2b
Nikhil Kumar Singh <connect.nikhilkumarsingh@gmail.com>
2026-09-21 23:28:11 +0530

    chore: initialize Next.js application with TypeScript

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main className="grow flex flex-col"></main>;
+}
---
+++ next.config.ts
@@ -0,0 +1,3 @@
+import type { NextConfig } from "next";
+
+export default { reactCompiler: true } satisfies NextConfig;
