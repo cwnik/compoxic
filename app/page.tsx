1e8477055dbcf59ce7b5579497bfc77defe754d4
Nikhil Kumar Singh <connect.nikhilkumarsingh@gmail.com>
2026-09-21 23:34:03 +0530

    chore: configure prettier and add formatting scripts

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="grow flex flex-col"></main>;
+    return <main className="flex grow flex-col"></main>;
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,8 @@ import nextTs from "eslint-config-next/typescript";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
+    ...nextVitals,
+    ...nextTs,
 
-  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+    globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"])
 ]);
