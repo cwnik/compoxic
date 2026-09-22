124c12e083241c779cc6073ac4b4397ede38ce44
Nikhil Kumar Singh <connect.nikhilkumarsingh@gmail.com>
2026-09-22 16:48:04 +0530

    feat: add global styles and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,28 @@
+import { Inter_Tight, JetBrains_Mono } from "next/font/google";
+import { cn } from "cn";
+
 import "./globals.css";
+
+const inter = Inter_Tight({
+    display: "swap",
+    variable: "--font-inter-tight",
+
+    subsets: ["latin"],
+    fallback: ["sans-serif"],
+    weight: ["400", "500", "600", "700"]
+});
+
+const mono = JetBrains_Mono({
+    display: "swap",
+    variable: "--font-jet-brains-mono",
+
+    subsets: ["latin"],
+    weight: ["400", "500"]
+});
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
-        <html lang="en" className="h-full antialiased">
+        <html lang="en" className={cn(inter.variable, mono.variable, "h-full antialiased")}>
             <body className="flex min-h-full flex-col">{children}</body>
         </html>
     );
