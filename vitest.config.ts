ea96bf6684358af1b20be0a8acefce05181d110f
Nikhil Kumar Singh <connect.nikhilkumarsingh@gmail.com>
2026-09-23 13:39:31 +0530

    test: setup vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+import path from "node:path";
+
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    resolve: { alias: { "@": path.dirname(fileURLToPath(import.meta.url)) } },
+    test: { environment: "jsdom", setupFiles: ["./vitest.setup.ts"] }
+});
