5b5f2b906a3f5b724c1e95b598edaad0024e6e67
Nikhil Kumar Singh <connect.nikhilkumarsingh@gmail.com>
2026-09-25 12:50:41 +0530

    feat: add IconActivity

--- features/icons/components/elements/activity.tsx
+++ features/icons/components/elements/activity.tsx
@@ -7,11 +7,9 @@ function IconActivity(props: IconProps) {
     return (
         <IconBase {...props}>
             <IconSecondary>
-                <path d="M2 12H6" />
-                <path d="M18 12H22" />
+                <rect x="1" y="1" width="22" height="22" rx="8" />
             </IconSecondary>
-
-            <path d="M6 12L8 10L10 14L12 2L14 22L18 12" />
+            <path d="M4.5 12L6.30574 12C7.28342 12 8.1178 11.2932 8.27853 10.3288L8.76701 7.39794C8.99912 6.00528 11.0009 6.00527 11.233 7.39794L12 12L12.767 16.602C12.9991 17.9947 15.0009 17.9947 15.233 16.602L15.7215 13.6712C15.8822 12.7068 16.7166 12 17.6943 12L19.5 12" />
         </IconBase>
     );
 }
@@ -26,7 +24,7 @@ const activity = defineIcon({
         category: "activity",
 
         primitives: ["path"],
-        tags: ["activity", "signal", "pulse"]
+        tags: ["activity", "pulse", "heartbeat", "ecg", "health", "monitoring", "signal"]
     },
     component: IconActivity
 });
